8ce9d8cde01b96d7efb21371b698ea30f7c8c86e
Carlos Roldan <carlosroldan26396@gmail.com>
2019-05-10 23:54:18 +0100

    Create 1557517747362_patient_schema.js

---
+++ medical-id/database/migrations/1557517747362_patient_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class PatientSchema extends Schema {
+  up () {
+    this.create('patients', (table) => {
+      table.increments()
+      table.integer('doctor_id').unsigned().references('id').inTable('users').notNullable()
+      table.string('name').notNullable()
+      table.string('dob').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('patients')
+  }
+}
+
+module.exports = PatientSchema
