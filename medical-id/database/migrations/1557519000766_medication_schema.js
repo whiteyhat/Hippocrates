14e2fc5a9de60109472db7ecad446c0e0b7f356e
Carlos Roldan <carlosroldan26396@gmail.com>
2019-05-10 23:54:22 +0100

    Create 1557519000766_medication_schema.js

---
+++ medical-id/database/migrations/1557519000766_medication_schema.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class MedicationSchema extends Schema {
+  up () {
+    this.create('medications', (table) => {
+      table.increments()
+      table.integer('patient_id').unsigned().references('id').inTable('patients').notNullable()
+      table.string('medication').notNullable()
+      table.string('dose').notNullable()
+      table.boolean('monday').notNullable().defaultTo(false)
+      table.boolean('tuesday').notNullable().defaultTo(false)
+      table.boolean('wednesday').notNullable().defaultTo(false)
+      table.boolean('thursday').notNullable().defaultTo(false)
+      table.boolean('friday').notNullable().defaultTo(false)
+      table.boolean('saturday').notNullable().defaultTo(false)
+      table.boolean('sunday').notNullable().defaultTo(false)
+      table.string('description').notNullable()
+
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('medications')
+  }
+}
+
+module.exports = MedicationSchema
