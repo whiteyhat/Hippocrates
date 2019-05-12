9a36753a9f28da438ad2d8f0aae9d10e21a7ad5e
Carlos Roldan <carlosroldan26396@gmail.com>
2019-05-12 15:34:57 +0100

    Update BlockchainService.js

--- app/Services/BlockchainService.js
+++ app/Services/BlockchainService.js
@@ -1,33 +1,42 @@
-
 //using the infura.io node, otherwise ipfs requires you to run a daemon on your own computer/server. See IPFS.io docs
 const IPFS = require('ipfs-api')
 const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
+
+//run with local daemon
+// const IPFS = require('ipfs-api');
+// const ipfs = new IPFS('localhost', '5001', {
+//   protocol: 'http'
+// });
+
 const Logger = use('Logger')
 const fs = require("fs")
 
-
-//run with local daemon
-// const ipfsApi = require('ipfs-api');
-// const ipfs = new ipfsApi('localhost', '5001', {protocol: 'http'});
 class BlockchainService {
 
   async uploadToIPFS(filename) {
-    let file = fs.readFileSync(filename);
+    Logger.info(filename)
+    try {        
+      return new Promise(resolve => {
 
-    let buffer = new Buffer.from(file);
-    Logger.info("Uploading file to IPFS")
-    //save document to IPFS,return its hash
-    //https://github.com/ipfs/interface-ipfs-core/blob/master/SPEC/FILES.md#add 
-    
-    return new Promise(resolve => {
-      ipfs.add(buffer, (err, ipfsHash) => {
-        resolve(ipfsHash[0].hash)
-      }) //await ipfs.add 
-    });
+      setTimeout(function(){
+        var content = fs.readFileSync(filename)
 
+        if (content) {
+          Logger.info("Uploading file to IPFS")
+          //save document to IPFS,return its hash
+          //https://github.com/ipfs/interface-ipfs-core/blob/master/SPEC/FILES.md#add 
+
+            ipfs.add(content, (err, ipfsHash) => {
+              Logger.info(ipfsHash)
+              resolve(ipfsHash[0].hash)
+            }) //await ipfs.add 
+        }
+      }, 1000);
+    })
+    } catch (error) {
+      Logger.error(error)
+    }
   }
-
-
 
 }
 
