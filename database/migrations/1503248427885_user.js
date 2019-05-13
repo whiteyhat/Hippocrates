ee9ba72f9db31362d86aec0c66226b3933a601d7
Carlos Roldan <carlosroldan26396@gmail.com>
2019-05-13 21:05:49 +0100

    Update 1503248427885_user.js

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -8,8 +8,15 @@ class UserSchema extends Schema {
     this.create('users', (table) => {
       table.increments()
       table.integer('nonce').unsigned().notNullable().defaultTo(Math.floor(Math.random() * 10000))
-      table.string('address').notNullable().unique()
+      table.string('wallet').notNullable().unique()
       table.boolean('admin').notNullable().defaultTo(0)
+      table.string('name')
+      table.string('role')
+      table.string('address')
+      table.string('phone')
+      table.string('email')
+      table.string('clinic')
+      table.binary('image')
       table.timestamps()
     })
   }
