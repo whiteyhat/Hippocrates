e5042def26a8c848924adb1833b6c4dbf6268245
Carlos Roldan <carlosroldan26396@gmail.com>
2019-05-17 13:35:33 +0100

    Update BlockchainService.js

--- app/Services/BlockchainService.js
+++ app/Services/BlockchainService.js
@@ -1,5 +1,5 @@
 //using the infura.io node, otherwise ipfs requires you to run a daemon on your own computer/server. See IPFS.io docs
-const IPFS = require('ipfs-api')
+const IPFS = require('ipfs-http-client')
 const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
 
 //run with local daemon
